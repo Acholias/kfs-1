46bc13cb8831ac159e2fb30c79a234c15ee1857b
Acholias <lucas.mugot@gmail.com>
2025-12-03 11:58:45 +0100

    add .nix for testing

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import inputs.nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          shell = pkgs.zsh;
+
+          buildInputs = [
+            pkgs.zsh
+            pkgs.mtools
+            pkgs.qemu
+            pkgs.grub2
+            pkgs.wget
+            pkgs.unzip
+          ];
+
+          shellHook = ''
+            exec zsh
+          '';
+        };
+      });
+}
